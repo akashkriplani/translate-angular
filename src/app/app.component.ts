2c3721615e19f178052e70907462558c564b3845
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-23 17:32:20 +0530

    Set default lang to en

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'translate-angular';
+  name: string = 'Akash';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 }
